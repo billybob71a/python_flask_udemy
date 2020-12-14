f2030ab47190267d2d63285f7554e0fba43f38dd
billybob71a <yungpet@gmail.com>
2020-12-13 19:10:41 -0700

    Update decorators_3.py

--- decorators_3.py
+++ decorators_3.py
@@ -4,7 +4,7 @@ we don't want to filter output based on a criteria
 '''
 import functools
 
-user = {"username": "jose", "access_level": "admijn"}
+user = {"username": "jose", "access_level": "admin"}
 
 def get_admin_password():
     return "1234"
@@ -12,9 +12,9 @@ def get_admin_password():
 def make_secure(func):
     #this is another decorator that tells secure_function that it is wrappper for func 
     @functools.wraps(func)
-    def secure_function():
+    def secure_function(panel):
         if user["access_level"] == "admin":
-            return func()
+            return func(panel)
     
     return secure_function
 
@@ -25,4 +25,6 @@ def get_password(panel):
     elif panel == "billing":
         return "super_secure_password"
 
-print(get_password())
+print(get_password("billing"))
+
+#"billing" gets passed to secure_fuction
