455d3d28aa78960b9bffc87b118959f5fdf49e2e
billybob71a <yungpet@gmail.com>
2020-12-14 18:26:04 -0700

    Create decorators_basic_concepts_2.py

---
+++ decorators_basic_concepts_2.py
@@ -0,0 +1,16 @@
+user = {"username": "jose", "access_level": "admin"}
+
+def make_secure(func):
+    def secure_function():
+        print("hello there")
+        if user["access_level"] == "admin":
+            return func()
+        else: 
+            return f"No admin permissions for {user['username']}"
+    return secure_function
+
+@make_secure
+def get_admin_password():
+    return "1234"
+
+print(get_admin_password())
